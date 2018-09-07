d1c2ab63a54e347d2c806845e64a191096bfdc5c
Hector Garcia <hectoregarciap@hotmail.com>
2018-09-07 14:25:20 -0500

    Fin session 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,20 +9,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
-
+  equipo: any[] = [];
   constructor(private http: HttpClient) {
-    //console.log('Hola servicio...')
+    this.cargaInfo();
+    this.cargaEquipo();
+  }
+
+  private cargaInfo() {
     //leer archivo json
     this.http.get("assets/data/data-pagina.json").subscribe((resp: InfoPagina) => {
 
       //console.log(resp['twitter']);
       this.cargada = true;
       this.info = resp;
-      console.log(resp.instagram);
-
-
+      
     });
+  }
 
-
+  private cargaEquipo() {
+    this.http.get("https://angular-html-1b339.firebaseio.com/equipo.json").
+      subscribe((resp:any) => {
+        this.equipo = resp;
+        
+      });
   }
 }
