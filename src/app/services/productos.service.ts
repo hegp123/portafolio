cc7cd890787cb59870b4136104e5c5a40dc5f128
Hector Garcia <hectoregarciap@hotmail.com>
2018-09-11 16:25:36 -0500

    listo para produccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,50 @@ export class ProductosService {
 
   cargando = true;
   producto: Producto[] = [];
+  productosFiltrados: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-1b339.firebaseio.com/productos_idx.json')
-      .subscribe((resp: Producto[]) => {
-        this.producto = resp;
-        this.cargando = false;
-      });
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-1b339.firebaseio.com/productos_idx.json')
+        .subscribe((resp: Producto[]) => {
+          this.producto = resp;
+          this.cargando = false;
+          resolve();
+        });
+    });
   }
 
-  public getProducto(id: String){
+  public getProducto(id: String) {
     return this.http.get(`https://angular-html-1b339.firebaseio.com/productos/${id}.json`);
+  }
+
+  public buscarProducto(termino: string) {
+    if (this.producto.length === 0) {
+      this.cargarProductos().then(() => {
+        //ejecutar despues de tener los productos
+        //aplicar filtro
+        this.filtrarProducto(termino);
+      });
+    } else {
+      //aplicar filtro
+      this.filtrarProducto(termino);
+    }
+
+
+  }
+
+  private filtrarProducto(termino: string) {
+    this.productosFiltrados = [];
+    termino = termino.toUpperCase();
+    this.producto.forEach(prod => {
+      if (prod.categoria.toLocaleUpperCase().indexOf(termino) >= 0 || prod.titulo.toLocaleUpperCase().indexOf(termino) >= 0) {
+        this.productosFiltrados.push(prod);
+      }
+    });
+    console.log(this.productosFiltrados);
   }
 }
